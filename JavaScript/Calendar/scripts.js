527b191747b61e590518610ddf456340b816c2b4
lysontrung <lysontrung95@gmail.com>
2017-06-27 09:11:45 +0700

    update calendar

--- JavaScript/Calendar/scripts.js
+++ JavaScript/Calendar/scripts.js
@@ -6,229 +6,225 @@ var months = new Array("January", "February", "March", "April", "May", "June", "
 var numberDayOfMonth = new Array("31", "28", "31", "30", "31", "30", "31", "31", "30", "31", "30", "31");
 var day = new Array("Sun", "Mon", "Tue", "Web", "Thu", "Fri", "Sat");
 var dateNow = new Date();
-var dateCheck = new Date().getdate();
+var dateCheck = new Date().getDate();
 var monthCheck = new Date().getMonth();
 var yearCheck = new Date().getFullYear();
 var positionDayOne = 0;
 var results = "";
 var positionDateNow;
 
-/*
-ham set ngay hien tai cho text input
-*/
-function setResult() {
-	results = dateCheck + "/" + (monthCheck + 1) + "/" + yearCheck;
-	document.getElementByid("output").values = results;
-	console.log(results);
+
+/*ham set ngay hien tai cho text input*/
+function results() {
+	results = dateCheck + "." + (monthCheck + 1) + "." + yearCheck;
+	document.getElementById("output").value = results;
 }
+/*---------------------------------------*/
 
 /*
 ham khoi tao bang ngay, thang, nam
 goi den hàm con tao cac btn, tao cac ngay trong nam, tao lich (2 tham so: thang, va nam)
 */
 function init() {
-	document.write("<div id='calendar' style='display:none;'>")
-		document.write("<form>");
-			document.write("<table id='table' style='text-align: center;'>");
-			this.drawBtns();
-			this.drawDay();
-			this.drawCalendar(dateNow.getMonth(), dateNow.getFullYear());
-			document.write("</table>");
-		document.write("</form>");
-	document.write("</div>")
+	document.write("<DIV ID='calendar' STYLE='display:none;'>")
+	document.write("<FORM>");
+	document.write("<TABLE ID='table' STYLE='text-align: center;'>");
+	this.drawRowBtn();
+	this.drawDay();
+	this.drawCalendar(dateNow.getMonth(), dateNow.getFullYear());
+	document.write("</TABLE>");
+	document.write("</FORM>");
+	document.write("</DIV>")
 }
+/*----------------------------------------*/
 
 /*--hàm khoi tao cac button (thang toi, thang lui, nam toi, nam lui, chon thang, chon nam) */
-	function drawBtns() {
-		document.write("<tr id='btns'>");
-		document.write("<td class='btn' onclick='btnPrevYear();'><a type='a' name='btnPreviousYear'>&#8647;</a></td>");
-		document.write("<td class='btn' onclick='btnPrevMonth();'><a type='a' name='btnPreviousMonth'>&larr;</a></td>");
-		document.write("<td class='btn' colspan='2'><select id='months' onchange='selectMonth();'>");
-		for (var i = 0; i < months.length; i++) {
-			document.write("<option values='" + i + "'>" + months[i] + "</option>")
-		};
-		document.write("</select></td>")
-		document.write("<td class='btn' colspan='1'><select id='years' onChange='selectYear();'>");
-		for (var i = 1900; i <= 2025; i++) {
-			document.write("<option values='" + i + "'>" + i + "</option>")
-		};
-		document.write("</select></td>")
-		document.write("<td class='btn' onClick='btnNextMonth();'><a type='a' name='btnNextMonth'>&rarr;</a></td>");
-		document.write("<td class='btn' onClick='btnNextYear();'><a type='a' name='btnNextYear' >&#8649;</a></td>");
-		document.write("</TR>");
-	}
-/*--*/
+function drawRowBtn() {
+	document.write("<TR ID='rowAction'>");
+	document.write("<TD CLASS='btn' onClick='btnPreviousYearClick();'><a TYPE='a' NAME='btnPreviousYear'>&#8647;</a></TD>");
+	document.write("<TD CLASS='btn' onClick='btnPreviousMonthClick();'><a TYPE='a' NAME='btnPreviousMonth'>&larr;</a></TD>");
+	document.write("<TD CLASS='btn' colspan='2'><SELECT ID='months' onChange='changeMonth();'>");
+	for (var i=0; i<months.length; i++) {
+		document.write("<OPTION VALUE='" + i + "'>" + months[i] + "</OPTION>")
+	};
+	document.write("</SELECT></TD>")
+	document.write("<TD CLASS='btn' colspan='1'><SELECT ID='years' onChange='changeYear();'>");
+	for (var i=1900; i<=2099; i++) {
+		document.write("<OPTION VALUE='" + i + "'>" + i + "</OPTION>")
+	};
+	document.write("</SELECT></TD>")
+	document.write("<TD CLASS='btn' onClick='btnNextMonthClick();'><a TYPE='a' NAME='btnNextMonth'>&rarr;</a></TD>");
+	document.write("<TD CLASS='btn' onClick='btnNextYearClick();'><a TYPE='a' NAME='btnNextYear' >&#8649;</a></TD>");
+	document.write("</TR>");
+}
+/*---------------------------------------*/
 
-/*ham khoi tao cac ngay trong calendar*/
-function drawDay() {
-		document.write("<TR id='title'>");
-		for (var i = 0; i < day.length; i++) {
-			document.write("<td>" + day[i] + "</td>");
-		};
-		document.write("</TR>");
-	}
-/*--*/
-	
 /*-- ham khoi tao calendar | voi 2 tham so truyen vao la (thang-nam)*/
-	function drawCalendar(month, year) {
-		var count = 1;
-		for (var i = 0; i < 6; i++) {
-			document.write("<TR onclick='closeCalendar();'>");
-			for (var j = 0; j < 7; j++) {
-				document.write("<td id='item" + count + "' class='item' onClick='checkDay(" + count + ");'></td>");		
-				count++;		
-			};
-			document.write("</TR>");
+function drawCalendar(month, year) {
+	var count = 1;
+	for (var i=0; i<6; i++) {
+		document.write("<TR onclick='closeCalendar();'>");
+		for (var j=0; j<7; j++) {
+			document.write("<TD ID='item" + count + "' CLASS='item' onClick='checkDay(" + count + ");'></TD>");		
+			count++;		
 		};
-		setdata();
-		setMonths();
-		setYears();
+		document.write("</TR>");
+	};
+	setData();
+	setMonths();
+	setYears();
+}
+/*----------------------------------------------------------------------*/
+	
+	
+/*xoa du kieu trong calendar
+42 tương ứng với số ô trong lịch
+*/
+function erase() {
+	for (var i=1; i<=42; i++) {
+		document.getElementById("item" + i).innerHTML = "";
+		document.getElementById("item" + i).style.background = "none";
 	}
-/*--*/
-
-/*xoa du kieu trong calendar*/
-	function erase() {
-		for (var i = 1; i <= 42; i++) {
-			document.getElementByid("item" + i).innerHTML = "";
-			document.getElementByid("item" + i).style.background = "none";
-		}
-	}
+}
+/*-------------------------*/
 
 /*set data cho calendar*/
-	function setdata() {
-		var dateNow = new Date().getdate();
-		var monthNow = new Date().getMonth();
-		var yearNow = new Date().getFullYear();
-		var day = new Date(yearCheck, monthCheck, 1).getday();
-		// get vi trí hien tai cua ngay
-		positionDayOne = parseInt(day);
-		var previous_month = monthCheck - 1;
-		var monthNext = monthCheck + 1;
+function setData() {
+	var dateNow = new Date().getDate();
+	var month_now = new Date().getMonth();
+	var yearNow = new Date().getFullYear();
+	var day = new Date(yearCheck, monthCheck, 1).getDay();
+	positionDayOne = parseInt(day);
+	var previous_month = monthCheck - 1;
+	var next_month = monthCheck + 1;
 		
-		// kiem tra co phai nam nhuan hay khong de tra ve so ngay phu hop
-		if (((yearCheck % 4 == 0) && (yearCheck % 100 != 0)) || (yearCheck % 400 == 0)) {
-			numberDayOfMonth[1] = 29; 
-		} else {
-			numberDayOfMonth[1] = 28;
-		}
+	/*hàm kiểm tra năm nhuận*/
+	if (((yearCheck % 4 == 0) && (yearCheck % 100 != 0)) || (yearCheck % 400 == 0)) {
+		numberDayOfMonth[1] = 29; 
+	} else {
+		numberDayOfMonth[1] = 28;
+	}
+	/*-------------------*/
 		
-		/*--xet vi tri cua ngay thang nam trong mang bat dau tu 0*/
-		if (previous_month < 0){
-			previous_month = 11;
-		}
-		if (monthNext == 12){
-			monthNext = 0;
-		}
-		/*--*/
-		
-		/*check ngay trong thang*/
-		var numberDay = numberDayOfMonth[monthCheck];
-		var numberDayOfPreviousMonth = numberDayOfMonth[previous_month];
-		var numberDayOfNextMonth = numberDayOfMonth[monthNext];
-		/*--*/
-		
-		/*set vi tri hien thi cua NGAY trong calendar*/
-		var position = 1;
-		var count = 1;
-		var check = false;
-		var positionStart = day;
-		var positionStop = (parseInt(numberDay) + parseInt(day));
-		/*--*/
+	/*--xet vi tri cua ngay thang nam trong mang bat dau tu 0*/
+	if (previous_month < 0){
+		previous_month = 11;
+	}
 
-		/*mang 2 chieu set vitri ngay trong calendar*/
-		for (var i = 0; i < 6; i++) {
-			for (var j = 0; j < 7; j++) {
-				if (count >= day) {
-					check = true;
-				}
-				if (check == true && position <= numberDay) {
-					document.getElementByid("item" + (position + day)).innerHTML = position;
-					document.getElementByid("item" + (position + day)).style.background = "#FBFCFC";
-					if (dateNow == position && monthNow == monthCheck && yearNow == yearCheck) {
-						document.getElementByid("item" + (position + day)).style.background = "#00ace6";
-						positionDateNow = "item" + (position + day);
-					}
-					document.getElementByid("item" + (position + day)).style.color = "#000";
-					position++;
-				}
-				count++;
+	if (next_month == 12){
+		next_month = 0;
+	}
+	/*--------------------------------------------------*/
+		
+	/*check ngay trong thang*/
+	var numberDay = numberDayOfMonth[monthCheck];
+	var numberDayOfPreviousMonth = numberDayOfMonth[previous_month];
+	var numberDayOfNextMonth = numberDayOfMonth[next_month];
+	/*----------------------------*/
+		
+	/*set vi tri hien thi cua NGAY trong calendar*/
+	var position = 1;
+	var count = 1;
+	var check = false;
+	var positionStart = day;
+	var positionStop = (parseInt(numberDay) + parseInt(day));
+	/*------------------------------------------*/
+		
+	/*mang 2chieu de hien thi ngay thang ra lich*/
+	for (var i=0; i<6; i++) {
+		for (var j=0; j<7; j++) {
+			if (count >= day) {
+				check = true;
 			}
-		}
-		/*42 la so o hien thi ngay*/
-		for (var i = positionStart; i>0; i--) {
-			document.getElementByid("item" + (i)).innerHTML = numberDayOfPreviousMonth--;
-			document.getElementByid("item" + (i)).style.color = "#ffffff";
-		}
-		for (var j=1; j <= (42 - positionStop); j++) {
-			document.getElementByid("item" + (j + positionStop)).innerHTML = j;
-			document.getElementByid("item" + (j + positionStop)).style.color = "#ffffff";
+			if (check == true && position <= numberDay) {
+				document.getElementById("item" + (position + day)).innerHTML = position;
+				document.getElementById("item" + (position + day)).style.background = "#FBFCFC";
+				if (dateNow == position && month_now == monthCheck && yearNow == yearCheck) {
+					document.getElementById("item" + (position + day)).style.background = "#00ace6";
+					positionDateNow = "item" + (position + day);
+				}
+				document.getElementById("item" + (position + day)).style.color = "#000";
+				position++;
+			}
+			count++;
 		}
 	}
-/*--*/
 	
+	for (var i=positionStart; i>0; i--) {
+		document.getElementById("item" + (i)).innerHTML = numberDayOfPreviousMonth--;
+		document.getElementById("item" + (i)).style.color = "#ffffff";
+	}
+	for (var j=1; j<=(42-positionStop); j++) {
+		document.getElementById("item" + (j + positionStop)).innerHTML = j;
+		document.getElementById("item" + (j + positionStop)).style.color = "#ffffff";
+	}
+}
+/*--------------------------*/
+
+
 /*ham kiem tra ngay trong thang*/
-	function checkDay(position) {
-		var previous_month = monthCheck;
-		var monthNow = monthCheck + 1;
-		var monthNext = monthCheck + 2
-		var year = yearCheck;
-		var day = document.getElementByid("item" + position).innerHTML;
+function checkDay(position) {
+	var previous_month = monthCheck;
+	var month_now = monthCheck + 1;
+	var next_month = monthCheck + 2
+	var year = yearCheck;
+	var day = document.getElementById("item" + position).innerHTML;
 
-		/*set vi tri cua THANG theo thu tu co trong mang*/
-		if (previous_month == 0) {
-			previous_month = 12;
+	if (previous_month == 0) {
+		previous_month = 12;
+	}
+
+	if (next_month == 13) {
+		next_month = 1;
+	}
+
+	if (parseInt(day) > position) {
+		if (previous_month == 12) {
+			year--;
 		}
-
-		if (monthNext == 13) {
-			monthNext = 1;
+		var results = day + "/" + previous_month + "/" + year;
+	} else if (parseInt(day) < (position - positionDayOne)) {
+		if (next_month == 1) {
+			year++;
 		}
-		/*--*/
-		
-		if (parseInt(day) > position) {
-			if (previous_month == 12) {
-				year--;
-			}
-			var results = day + "/" + previous_month + "/" + year;
-		} else if (parseInt(day) < (position - positionDayOne)) {
-			if (monthNext == 1) {
-				year++;
-			}
-			var results = day + "/" + monthNext + "/" + year;
-		} else {
-			var results = day + "/" + monthNow + "/" + year;
-		}
-
-		document.getElementByid("output").values = results;
-	}
-/*--*/
-
-/*ham thay doi va set lai gia tri cho thang-nam*/
-	function selectMonth() {
-		erase();
-		monthCheck = document.getElementByid("months").values;
-		console.log(monthCheck);
-		setdata();
+		var results = day + "/" + next_month + "/" + year;
+	} else {
+		var results = day + "/" + month_now + "/" + year;
 	}
 
-	function setMonths() {
-		document.getElementByid("months").values = monthCheck;
-	}
-	
-	function selectYear() {
-		erase();
-		yearCheck = document.getElementByid("years").values;
-		console.log(monthCheck);
-		setdata();
-	}
-	
-	function setYears() {
-		document.getElementByid("years").values = yearCheck;
-	}
-/*--*/
-	
+	document.getElementById("output").value = results;
+}
+/*------------------------*/
 
-/*ham event cho btn preMonth*/
-	function btnPrevMonth() {
+/*kiem tra THANG va set lai data*/
+function changeMonth() {
+	erase();
+	monthCheck = document.getElementById("months").value;
+	console.log(monthCheck);
+	setData();
+}
+
+function setMonths() {
+	document.getElementById("months").value = monthCheck;
+}
+/*-------------------------------*/
+
+/*kiem tra lai NAM va set lai data*/
+function changeYear() {
+	erase();
+	yearCheck = document.getElementById("years").value;
+	console.log(monthCheck);
+	setData();
+}
+
+function setYears() {
+	document.getElementById("years").value = yearCheck;
+}
+/*------------------*/
+
+
+/*ham set event cho btn preMonth*/
+	function btnPreviousMonthClick() {
 		erase();
 		monthCheck = monthCheck - 1;
 		if (monthCheck < 0) {
@@ -236,14 +232,15 @@ function drawDay() {
 			yearCheck = yearCheck - 1;
 		}
 		console.log(monthCheck);
-		setdata();
+		setData();
 		setMonths();
 		setYears();
 	}
-/*--*/
+/*-----------------------------*/	
 
-/*ham event cho btn nextMonth*/
-	function btnNextMonth() {
+
+/*ham set event cho btn nextMonth*/
+	function btnNextMonthClick() {
 		erase();
 		monthCheck = monthCheck + 1;
 		if (monthCheck > 11) {
@@ -251,44 +248,56 @@ function drawDay() {
 			yearCheck = yearCheck + 1;
 		}
 		console.log(monthCheck);
-		setdata();
+		setData();
 		setMonths();
 		setYears();
 	}
-/*--*/
-
-/*ham event cho btn preYear*/
-	function btnPrevYear() {
+/*-----------------------------------*/
+	
+/*ham set event cho btn preYear*/
+	function btnPreviousYearClick() {
 		erase();
 		yearCheck = yearCheck - 1;
 		console.log(monthCheck);
-		setdata();
+		setData();
 		setMonths();
 		setYears();
 	}
-/*--*/
+/*------------------------------------*/
 
-/*ham event cho btn nextYear*/
-	function btnNextYear() {
+/*ham set event cho btn nextYear*/
+	function btnNextYearClick() {
 		erase();
 		yearCheck = yearCheck + 1;
 		console.log(monthCheck);
-		setdata();
+		setData();
 		setMonths();
 		setYears();
 	}
-/*--*/
+/*----------------------------*/
 
-	function setItem(item, values) {
-		var item = document.getElementByid(item);
-		item.innerHTML = values;
+
+/*khoi tao ngay trong calendar*/
+	function drawDay() {
+		document.write("<TR ID='title'>");
+		for (var i=0; i<day.length; i++) {
+			document.write("<TD>" + day[i] + "</TD>");
+		};
+		document.write("</TR>");
 	}
-/*ham dong va mo calendar*/
+/*--------------------*/
+
+	function setItem(item, value) {
+		var item = document.getElementById(item);
+		item.innerHTML = value;
+	}
+
+/*đóng và mở form*/
 	function closeCalendar() {
-		document.getElementByid("calendar").style.display = "none";
+		document.getElementById("calendar").style.display = "none";
 	}
-
+	/*không cần thiết*/
 	function openCalendar() {
-		document.getElementByid("calendar").style.display = "block";
+		document.getElementById("calendar").style.display = "block";
 	}
-/*--*/
+/**/
